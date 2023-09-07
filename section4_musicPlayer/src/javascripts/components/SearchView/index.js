90eb5cd88a2b739cdf225d000be565ebce5dfc4b
81670 <jiwook@81670.local>
2023-09-07 22:50:45 +0900

    searchlist play bug fix

--- section4_musicPlayer/src/javascripts/components/SearchView/index.js
+++ section4_musicPlayer/src/javascripts/components/SearchView/index.js
@@ -79,7 +79,7 @@ export default class SearchView extends EventEmitter {
   requestPlay(target) {
     const controller = target.parentElement;
     const { index: musicIndex } = controller.dataset;
-    const payload = { musics: this.musics, musicIndex };
+    const payload = { musics: this.searchedMusics, musicIndex };
     this.emit("play", payload);
     this.renderStopAll();
     target.classList.replace("icon-play", "icon-pause");
@@ -95,7 +95,7 @@ export default class SearchView extends EventEmitter {
   requestAddPlayList(target) {
     const controller = target.parentElement;
     const { index: musicIndex } = controller.dataset;
-    const payload = { musics: this.musics, musicIndex };
+    const payload = { musics: this.searchedMusics, musicIndex };
     this.emit("addPlayList", payload);
   }
 
